3a95c91c96b5284d87624641bc43ffe6bcd83559
mnh <martin.hylleberg@systematic.com>
2017-01-16 14:01:54 +0100

    Session 6 lesson 2

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,8 +2,19 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
+import { AngularFireModule } from 'angularfire2';
 
 import { AppComponent } from './app.component';
+
+
+// Must export the config
+export const firebaseConfig = {
+  apiKey: 'AIzaSyAf-Ib4WRX6tayh0XTUETlxUDhu6W6ro0E',
+  authDomain: 'businesscontacts-fb34b.firebaseapp.com',
+  databaseURL: 'https://businesscontacts-fb34b.firebaseio.com',
+  storageBucket: 'businesscontacts-fb34b.appspot.com',
+  messagingSenderId: '741416000228'
+};
 
 @NgModule({
   declarations: [
@@ -12,7 +23,8 @@ import { AppComponent } from './app.component';
   imports: [
     BrowserModule,
     FormsModule,
-    HttpModule
+    HttpModule,
+    AngularFireModule.initializeApp(firebaseConfig)
   ],
   providers: [],
   bootstrap: [AppComponent]
